af0f6e8391726161e5a1d03a0e3b84a0d4b26d67
toufiqulislamtanmoy <tittanmoy@gmail.com>
2023-04-07 13:03:28 +0600

    fake data did not work problem solve

--- user/Includes/head.php
+++ user/Includes/head.php
@@ -6,6 +6,10 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Home</title>
+  <!-- Favicon -->
+  <!-- Favicon icon -->
+  <link rel="icon" href="../../assests/img/icons/favicon.png" type="image/png">
+
   <!-- Bootstrap CDN link -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
