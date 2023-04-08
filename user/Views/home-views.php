b1b84ea2673508dfb79263b2cf96d4aa1e39d066
samia5725 <samia665725@gmail.com>
2023-04-08 22:58:28 +0600

    add image link

--- user/Views/home-views.php
+++ user/Views/home-views.php
@@ -9,7 +9,7 @@
       "industry": "Construction",
       "rating": 4.5,
       "gender": "Male",
-      "profile-image": ""
+      "profile-image": "https://images.unsplash.com/photo-1534030347209-467a5b0ad3e6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
     },
     {
       "id": 2,
