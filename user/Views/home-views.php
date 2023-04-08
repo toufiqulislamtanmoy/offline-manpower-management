d25ff9f4ec650acbc6a9a6ef7772ac282b237480
toufiqulislamtanmoy <tittanmoy@gmail.com>
2023-04-08 23:06:42 +0600

    solve the error

--- user/Views/home-views.php
+++ user/Views/home-views.php
@@ -9,7 +9,7 @@
       "industry": "Construction",
       "rating": 4.5,
       "gender": "Male",
-      "profileImage": ""
+      "profileImage": "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
     },
     {
       "id": 2,
